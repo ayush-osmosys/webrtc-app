ba39ce95ccb8283a339e2d25642d0f788032507e
ayush-osmosys <ayush@osmosys.co>
2023-07-21 17:16:49 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 
 import env from "./env.config.js";
@@ -7,7 +8,17 @@ import env from "./env.config.js";
 const PORT = env.PORT;
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors);
 
 server.listen(PORT, () => console.log("listening on port " + PORT));
-io.on("connection", () => console.log("A Connection established"));
+io.on("connection", (socket) => {
+  console.log("A Connection established");
+  socket.on("disconnect", () => console.log("Disconnected"));
+});

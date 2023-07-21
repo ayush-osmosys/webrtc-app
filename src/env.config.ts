2cca3b6f8792c1b275460fa710a8fe7d9e166117
ayush-osmosys <ayush@osmosys.co>
2023-07-21 13:24:24 +0530

    add env variables with envalid

---
+++ src/env.config.ts
@@ -0,0 +1,9 @@
+import { cleanEnv, port, url } from "envalid";
+import dotenv from "dotenv";
+dotenv.config();
+
+const env = cleanEnv(process.env, {
+  DATABASE_URL: url(),
+  PORT: port({ default: 4000 }),
+});
+export default env;

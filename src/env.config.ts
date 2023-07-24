9f98481d819f7918e185fefd450bf43a365ef20e
ayush-osmosys <ayush@osmosys.co>
2023-07-24 14:28:17 +0530

    remove unnessecary variables from env

--- src/env.config.ts
+++ src/env.config.ts
@@ -1,9 +1,8 @@
-import { cleanEnv, port, url } from "envalid";
+import { cleanEnv, port } from "envalid";
 import dotenv from "dotenv";
 dotenv.config();
 
 const env = cleanEnv(process.env, {
-  // DATABASE_URL: url(),
   PORT: port({ default: 4000 }),
 });
 export default env;

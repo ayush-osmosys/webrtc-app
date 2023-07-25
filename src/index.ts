7c13415eac6cc36d47455f4b2900cbf6cb33aee3
ayush-osmosys <ayush@osmosys.co>
2023-07-25 15:14:01 +0530

    add event handler for room join and disconnect

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,13 @@ app.use(homeRouter);
 
 io.on("connection", (socket) => {
   console.log("A Connection established");
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+    console.log(roomId, userId);
+  });
   socket.on("disconnect", () => console.log("Disconnected"));
 });

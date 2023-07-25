6f5176c073822cc235eb4cb4c9a7451b9f980363
ayush-osmosys <ayush@osmosys.co>
2023-07-25 15:14:26 +0530

    shorten room id

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { v4 as uuid } from "uuid";
+import base64url from "base64url";
+import { createHash } from "crypto";
+
 const router = Router();
 
+function generateShortIdentifier(input: string, length: number) {
+  const hash = createHash("sha256").update(input).digest("hex");
+  return hash.substr(0, length);
+}
+
 router.get("/", (req, res) => {
-  res.redirect(`/${uuid()}`);
+  const shortId = generateShortIdentifier(uuid(), 5);
+  res.redirect(`/${shortId}`);
 });
 
 router.get("/:roomid", (req, res) => {
